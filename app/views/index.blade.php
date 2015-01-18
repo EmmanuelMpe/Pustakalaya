611cbc23d2c670400b98cbaceffe860c48e9a5a7
tnagorra <weathermist@gmail.com>
2015-01-18 15:28:02 +0545

    Added some template files

---
+++ app/views/index.blade.php
@@ -0,0 +1,112 @@
+@extends('layout.basic') @section('content') @include('layout.navbar')
+<div class="container">
+
+  <div class="row">
+    <div class="col-lg-8 col-md-7 col-sm-6">
+      <h2>Student</h2>
+    </div>
+  </div>
+
+  <div class="lend-table">
+    <div class="row">
+      <div class="col-lg-12">
+        <table class="table table-striped table-hover ">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Book</th>
+              <th>Author</th>
+              <th>Edition</th>
+              <th>Date Issued</th>
+              <th>Expiry Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- tr class="info | success | danger | warning | active" -->
+            <tr>
+              <td>1038</td>
+              <td>Into the wild</td>
+              <td>Me</td>
+              <td>2010</td>
+              <td>2014-12-25</td>
+              <td>2015-02-25</td>
+            </tr>
+            <tr>
+              <td>1038</td>
+              <td>Into the wild</td>
+              <td>Me</td>
+              <td>2010</td>
+              <td>2014-12-25</td>
+              <td>2015-02-25</td>
+            </tr>
+            <tr class="danger">
+              <td>1038</td>
+              <td>Into the wild</td>
+              <td>Me</td>
+              <td>2010</td>
+              <td>2014-12-25</td>
+              <td>2015-02-25</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-8 col-md-7 col-sm-6">
+      <h2>Librarian</h2>
+      <!-- <p class="lead">hehe</p> -->
+    </div>
+  </div>
+
+  <div class="transaction-form">
+    <div class="row">
+      <div class="col-lg-8">
+        <form class="form-horizontal">
+          <fieldset>
+            <legend>Issue, Renew or Return books</legend>
+            <div class="form-group">
+              <label for="inputEmail" class="col-lg-2 control-label">Username</label>
+              <div class="col-lg-10">
+                <input class="form-control" id="inputEmail" placeholder="Username" type="text">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="textArea" class="col-lg-2 control-label">Books</label>
+              <div class="col-lg-10">
+                <textarea class="form-control" rows="3" id="inputBook"></textarea>
+                <span class="help-block">Enter book ids seperated by a semicolon.</span>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-lg-10 col-lg-offset-2">
+                <button type="submit" class="btn btn-primary">Renew</button>
+                <button type="submit" class="btn btn-success">Issue</button>
+                <button type="submit" class="btn btn-warning">Return</button>
+                <button class="btn btn-default pull-right">Cancel</button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+      <div class="col-lg-4">
+        <div class="alert alert-dismissable alert-success">
+          <button type="button" class="close" data-dismiss="alert">×</button>
+          <strong>Gone with the wind</strong> has been renewed.</a>
+        </div>
+
+        <div class="alert alert-dismissable alert-danger">
+          <button type="button" class="close" data-dismiss="alert">×</button>
+          <strong>To kill a mocking bird</strong> can't be issued. It is already issued.</a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+
+</div>
+@stop
---
+++ app/views/layout/navbar.blade.php
@@ -0,0 +1,43 @@
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="#" class="navbar-brand">Pustakalaya </a>
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="navbar-collapse collapse" id="navbar-main">
+        <ul class="nav navbar-nav">
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Books <span class="caret"></span></a>
+            <ul class="dropdown-menu" aria-labelledby="themes">
+              <li><a href="#">Course </a>
+              </li>
+              <li class="divider"></li>
+              <li><a href="#">Reference</a>
+              </li>
+              <li><a href="#">Other</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+    <form class="navbar-form navbar-left">
+      <input class="form-control col-lg-8" placeholder="Search" type="text">
+    </form>
+    <ul class="nav navbar-nav navbar-right">
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#" target="_blank">About</a>
+          </li>
+          <li>
+            <a href="#">Help </a>
+          </li>
+          <li><a href="#">Sign Out</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </div>
