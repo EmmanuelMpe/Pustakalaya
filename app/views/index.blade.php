1cf156690091d801627a8480de37332e08cf057d
tnagorra <weathermist@gmail.com>
2015-01-18 15:41:01 +0545

    Added homepage for verifier

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -11,6 +11,7 @@
     <div class="row">
       <div class="col-lg-12">
         <table class="table table-striped table-hover ">
+          <legend>Lent Books</legend>
           <thead>
             <tr>
               <th>Id</th>
@@ -54,6 +55,10 @@
     </div>
   </div>
 
+  <hr>
+
+
+
   <div class="row">
     <div class="col-lg-8 col-md-7 col-sm-6">
       <h2>Librarian</h2>
@@ -80,6 +85,7 @@
                 <span class="help-block">Enter book ids seperated by a semicolon.</span>
               </div>
             </div>
+
             <div class="form-group">
               <div class="col-lg-10 col-lg-offset-2">
                 <button type="submit" class="btn btn-primary">Renew</button>
@@ -88,6 +94,7 @@
                 <button class="btn btn-default pull-right">Cancel</button>
               </div>
             </div>
+
           </fieldset>
         </form>
       </div>
@@ -105,6 +112,62 @@
       </div>
     </div>
   </div>
+
+    <hr>
+
+  <div class="row">
+    <div class="col-lg-8 col-md-7 col-sm-6">
+      <h2>Verifier</h2>
+      <!-- <p class="lead">hehe</p> -->
+    </div>
+  </div>
+
+  <div class="transaction-form">
+    <div class="row">
+      <div class="col-lg-8">
+        <form class="form-horizontal">
+          <fieldset>
+            <legend>Verify books</legend>
+            <div class="form-group">
+              <label for="inputEmail" class="col-lg-2 control-label">Username</label>
+              <div class="col-lg-10">
+                <input class="form-control" id="inputEmail" placeholder="Username" type="text">
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="textArea" class="col-lg-2 control-label">Books</label>
+              <div class="col-lg-10">
+                <textarea class="form-control" rows="3" id="inputBook"></textarea>
+                <span class="help-block">Enter book ids seperated by a semicolon.</span>
+              </div>
+            </div>
+
+           <div class="form-group">
+              <div class="col-lg-10 col-lg-offset-2">
+                <button type="submit" class="btn btn-primary">Verify</button>
+                <button class="btn btn-default pull-right">Cancel</button>
+              </div>
+            </div>
+
+          </fieldset>
+        </form>
+      </div>
+      <div class="col-lg-4">
+        <div class="alert alert-dismissable alert-success">
+          <button type="button" class="close" data-dismiss="alert">×</button>
+          <strong>Gone with the wind</strong> has been issued.</a>
+        </div>
+
+        <div class="alert alert-dismissable alert-danger">
+          <button type="button" class="close" data-dismiss="alert">×</button>
+          <strong>To kill a mocking bird</strong> hasn't be issued</a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+
 
 </div>
 
