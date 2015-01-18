36330d912bbcf78930eb4556c4ca2aa84b4afc04
tnagorra <weathermist@gmail.com>
2015-01-18 23:22:22 +0545

    Some changes to footer

--- app/views/layout/footer.blade.php
+++ app/views/layout/footer.blade.php
@@ -1,7 +1,5 @@
 <footer>
-  <div class="row">
-    <div class="col-lg-12">
-
+  <div class="container well">
       <ul class="list-unstyled">
         <li class="pull-right">
           <a href="#top">Back to top</a>
@@ -12,9 +10,7 @@
         <li><a href="#">Support</a>
         </li>
       </ul>
-
       <p>Made by <a href="#" rel="nofollow">GoodGuys</a>. Contact us at <a href="mailto:hehe@hehe.com">hehe@hehe.com</a>.</p>
       <p>Copyright 2015-2099. All rights reserved.</p>
-    </div>
   </div>
 </footer>
