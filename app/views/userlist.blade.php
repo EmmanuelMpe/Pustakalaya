f3c1f39cb3b074095dff868f0f97cee386f8cc3e
पुष्प राज <social@pusparaaz.com.np>
2015-01-21 12:59:52 +0545

    userlist selection added

--- app/views/userlist.blade.php
+++ app/views/userlist.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row">
   <div class="col-lg-8 col-md-7 col-sm-6">
-    <h2>Admin<span class="lead">/Puspe</span></h2>
+    <h2>Admin<span class="lead">/Puspe (This page needs major designing changes and improvements.)</span></h2>
   </div>
 </div>
 
@@ -22,9 +22,56 @@
 
   <div class="col-lg-10">
     <div class="row">
-      <div class="col-lg-8 col-md-7 col-sm-6">
+      <div class="col-lg-2 col-md-4 col-sm-3">
         <h2>069/BCT</h2>
       </div>
+	<div class="col-lg-8 pull-right">
+	  <form class="form">
+	    <fieldset>
+	      <div class="form-group">
+	        <label for="inputType" class="col-lg-1 control-label">Type:</label>
+	        <div class="col-lg-2">
+	          <select id="inputType">
+	          	<option>Staff</option><option>Student</option>
+	          </select>
+	        </div>
+	      </div>
+	      <!-- Authority will be visible and batch and programs will be hidden when staff is selected.
+	      		The view will be reversed for the selection of student. -->
+	      <div class="form-group">
+	        <label for="inputAuthority" class="col-lg-2 control-label">Authority:</label>
+	        <div class="col-lg-3">
+	          <select id="inputAuthority">
+	          	<option>Administrator</option><option>Librarian</option><option>Verifier</option>
+	          </select>
+	        </div>
+	      </div>
+	      <div class="form-group hidden">
+	        <label for="inputBatch" class="col-lg-1 control-label">Batch:</label>
+	        <div class="col-lg-2">
+	          <select id="inputBatch">
+	          	<option>2068</option><option>2069</option><option>2070</option><option>2071</option>
+	          </select>
+	        </div>
+	      </div>
+	      <div class="form-group hidden">
+	        <label for="inputProgram" class="col-lg-1 control-label">Program:</label>
+	        <div class="col-lg-2">
+	          <select id="inputProgram">
+	          	<option>BAE</option><option>BCE</option><option>BCT</option>
+	          	<option>BEL</option><option>BEX</option><option>BME</option>
+	          </select>
+	        </div>
+	      </div>
+	      <div class="form-group">
+	        <div class="col-lg-2">
+	          <button type="submit" class="btn btn-primary">Filter</button>
+	        </div>
+	      </div>
+
+	    </fieldset>
+	  </form>
+	</div>
     </div>
     <div class="recommendation-table ">
       <div class="col-lg-6">
