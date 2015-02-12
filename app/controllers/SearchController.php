91919a0534185e01c0dbbcec0e1d46cb9c2c7175
Neeraj Adhikari <adh.neeraj@gmail.com>
2015-02-12 09:18:26 +0545

    Search Books via query

--- app/controllers/SearchController.php
+++ app/controllers/SearchController.php
@@ -8,12 +8,15 @@ class SearchController extends \BaseController {
   public function postIndex() {
     // Build validation rules
     $rules = array('query_type'=>'required|in:User,Book',
-      'book_type'=>'in:Lendable,NonLendable,Reference',
-      'user_type'=>'in:Student,Verifier,Admin');
+      'book_type'=>'in:Lendable,NonLendable,Reference,All',
+      'user_type'=>'in:Student,Verifier,Admin,All');
+    //return Input::all();
 
     // Validate
     $validator = Validator::make(Input::all(),$rules);
     $messages = array();
+
+    $query = Input::get('query','');
 
     // If validation fails, construct messages and bail out
     if ($validator->fails()) {
@@ -26,12 +29,21 @@ class SearchController extends \BaseController {
     $querytype = Input::get('query_type');
     if ($querytype=='Book') {
       $type = Input::get('book_type');
-      if ($type=='')
-        $books = Book::paginate(10);
-      else
-        $books = Book::where('type_name','=',$type)->paginate(10);
-      return View::make('book.list')->withBooks($books);
+      if ($type=='' | $type=='All') {
+        $books = Book::where('name','like',"%".$query."%")->
+          orWhere('author','like',"%".$query."%")->
+          orWhere('publisher','like',"%".$query."%")->
+          paginate(10);
+      } else {
+        $books = Book::where('type_name','=',$type)->
+          where('name','like',"%".$query."%")->
+          orWhere('author','like',"%".$query."%")->
+          orWhere('publisher','like',"%".$query."%")->
+          paginate(10);
+      }
+        return View::make('book.list')->withBooks($books);
     } elseif ($querytype=='User') {
+      return Input::all();
       $type = Input::get('user_type');
       if ($type=='')
         $users = User::paginate(10);
